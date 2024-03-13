b5532d7e61c7946b4399470a7b2c9c2866a4c40e
foxane <65512187+foxane@users.noreply.github.com>
2024-03-14 03:53:16 +0700

    update
    fuck you js

--- js-algorithm-data-structure/roman-numeral-converter/script.js
+++ js-algorithm-data-structure/roman-numeral-converter/script.js
@@ -1,45 +1,40 @@
-const input = document.getElementById('number');
-const btn = document.getElementById('convert-btn');
-const output = document.getElementById('output');
-
-const reference = [
-    [1000, 'M'],
-    [900, 'CM'],
-    [500, 'D'],
-    [400, 'CD'],
-    [100, 'C'],
-    [90, 'XC'],
-    [50, 'L'],
-    [40, 'XL'],
-    [10, 'X'],
-    [9, 'IX'],
-    [5, 'V'],
-    [4, 'IV'],
-    [1, 'I']
-  ];
+const input = document.getElementById("number");
+const btn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+let roman =[];
+let romanStr;
 
 const convert = num => {
-    if (input.value === 0) {
-        output.innerText = 'Please enter a number between 1 - 3999'
-        output.classList.add('alert')
-        return ''
+    input.value = '';
+  const reference = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+  for (var i = 0; i < reference.length ; i++) {
+    if (num >= reference[i][0]) {
+        roman.push(reference[i][1])
+        console.log(roman);
+      return reference[i][1] + convert(num - reference[i][0]);
     }
-    output.classList.remove('alert')
-    for (var i = 0; i < reference.length; i++) {
-        if (num >= reference[i][0]) {
-            return reference[i][1] + convert(num - reference[i][0]);
-        }
-    }
-}
+  }
+};
 
-
-btn.addEventListener('click', () => {
-    output.innerText = convert(input.value)
+/* Event initialize */
+btn.addEventListener("click", () => {
+    convert(input.value)
     output.classList.remove('hidden')
-})
-input.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-    output.classList.remove('hidden')
-    output.classList.toggle('alert')
-    }
-})
+    /* TODO: turn roman to string and remove commas */
+    romanStr = roman.split(',').join('')
+    output.innerText = romanStr
+});
