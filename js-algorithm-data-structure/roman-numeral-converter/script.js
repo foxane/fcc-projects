ca3cd25907f0729f035037e0a6a981c63a0409b3
foxane <65512187+foxane@users.noreply.github.com>
2024-03-14 01:50:55 +0700

    js initialized

--- js-algorithm-data-structure/roman-numeral-converter/script.js
+++ js-algorithm-data-structure/roman-numeral-converter/script.js
@@ -1,4 +1,6 @@
-
+const input = document.getElementById('number');
+const btn = document.getElementById('convert-btn');
+const output = document.getElementById('output');
 
 const reference = [
     [1000, 'M'],
