9b3ee24f5029a0d6ccfe3fac7d15f8862bdf40ab
foxane <65512187+foxane@users.noreply.github.com>
2024-03-14 21:09:14 +0700

    added funstions

--- js-algorithm-data-structure/roman-numeral-converter/script.js
+++ js-algorithm-data-structure/roman-numeral-converter/script.js
@@ -2,7 +2,7 @@ const input = document.getElementById("number");
 const btn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
-const reference = [
+const ref = [
   [1000, "M"],
   [900, "CM"],
   [500, "D"],
@@ -18,3 +18,39 @@ const reference = [
   [1, "I"],
 ];
 
+const toRoman = (int) => {
+  if (int <= 0) {
+    return ''
+  }
+  for (var i = 0; i < ref.length; i++) {
+    if(int >= ref[i][0]) {
+      return ref[i][1] + toRoman(int - ref[i][0])
+    }
+  }
+}
+
+const isValid = (str, int) => {
+  let msg = ''
+  if (!str || str.match(/[e.]/g)) {
+      msg = 'Please enter a valid number'
+  } else if (int <= 0) {
+      msg = 'Roman numerals dont have 0'
+  } else if (int > 3999) {
+      msg = 'Roman numerals have max of 3999'
+  } else {
+      return true;
+  }
+  output.classList.add('alert')
+  output.innerText = msg;
+  return false;
+}
+
+btn.addEventListener('click', () => {
+  const numStr = input.value;
+  const int = parseInt(numStr, 10);
+
+  output.classList.remove('hidden')
+  if (isValid(numStr, int)) {
+      output.innerText = toRoman(int);
+  }
+})
--- js-algorithm-data-structure/roman-numeral-converter/task.md
+++ js-algorithm-data-structure/roman-numeral-converter/task.md
@@ -1,7 +1,7 @@
-1. Store user input as string and int
-2. validate user input 
+1. Store user input as string and int done
+2. validate user input  done 
    * if str have \e+-.\g = "Please enter a valid number"
    * if int < 0 = "Roman numerals don't have 0. Please input at least 1"
    * if int > 3999 = "Roman numerals have a maximum value of 3999, please enter a number between 1 - 3999"
-3. Run recursive roman converter
+3. Run recursive roman converter 
 4. Update ui 
